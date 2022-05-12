31564d8cf97f30edb7501e8d8bc169c3646aaee9
Masum Raza <masumrazait@gmail.com>
2022-05-12 23:57:34 +0530

    Add ScreenShot Code full Page & Element

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -26,10 +26,25 @@ fixture `Getting started with TestCafe`
 test('My First testCafe test', async t =>{
 
     // here goes testCafe code
-    await t.typeText('#developer-name',"Md Bittu Ansari")
-    await t.typeText('#comments',"Hello Tester")
-    await t.click('#submit-button')
+    const developer_name_input = Selector('#developer-name')
+    const developer_comments = Selector('#comments')
+    const submit_button = Selector('#submit-button')
+    const articleText = Selector('#article-header').innerText
+
+    //take screenShot
+    //await t.takeScreenshot({fullPage:true})
+    //take ScreenShot of Element
+    await t.takeElementScreenshot(submit_button)
+
+    await t.typeText(developer_name_input,'Md Bittu Ansari')
+    await t.typeText(developer_comments,'Hello Developer')
+    await t.click(submit_button)
+    await t.expect(articleText).contains("Md Bittu")
+
+    // line number 29,30 or you can write the code in one line like line no 33
+    //await t.typeText('#developer-name',"Md Bittu Ansari")
     
+
     //wait but its not good way 
     await t.wait(3000)
 
