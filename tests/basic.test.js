480176def8f8931eaadfab64df4c5fc9183346f8
Masum Raza <masumrazait@gmail.com>
2022-05-12 23:38:06 +0530

    updated code

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -4,14 +4,35 @@ import { Selector } from 'testcafe'
 fixture `Getting started with TestCafe`
     .page `https://devexpress.github.io/testcafe/example/`
 
+    //Hook before, beforeEach, after, afterEach
+    .before(async t=>{
+        //Test setup goes here
+        //await runDatabaseReset()
+        //await seedTestData()
+
+    })
+    .beforeEach(async t=> {
+        //Runs Before each test    
+        await t.setTestSpeed(1)             //for debug the ui speed we need to declear speed 
+    })
+    .after(async t=> {
+        //Cleaning test Data
+        //Loggon and sending data to monitoring systems
+    })
+    .afterEach(async t=> {
+        //Runs after each test
+    })
+
 test('My First testCafe test', async t =>{
 
     // here goes testCafe code
-    await t.typeText('#developer-name',"Mahi RAZA")
+    await t.typeText('#developer-name',"Md Bittu Ansari")
     await t.typeText('#comments',"Hello Tester")
     await t.click('#submit-button')
+    
+    //wait but its not good way 
+    await t.wait(3000)
 
     //Expect Comparison
-    
-    await t.expect(Selector('#article-header').innerText).contains('Mahi')
+    await t.expect(Selector('#article-header').innerText).contains('Bittu')
 })
