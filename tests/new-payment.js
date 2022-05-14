4cc83687066ec50ca74841e86c1abc4d0f14ad14
Masum Raza <masumrazait@gmail.com>
2022-05-14 18:49:43 +0530

    added helper.js and new payment page test Script

---
+++ tests/new-payment.js
@@ -0,0 +1,10 @@
+import { fixture, Selector } from "testcafe";
+import { login } from "./helper";
+
+//prettier-ignore
+fixture `New payment test`
+.page `http://zero.webappsecurity.com/index.html`
+
+test.before(async t=>{
+    await login('username','password');
+})
