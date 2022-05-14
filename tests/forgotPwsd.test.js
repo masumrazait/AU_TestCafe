9039ce862cc56209af427c7d0dc68784703dac3b
Masum Raza <masumrazait@gmail.com>
2022-05-14 17:50:08 +0530

    added login and forgot password page test Script

---
+++ tests/forgotPwsd.test.js
@@ -0,0 +1,51 @@
+import { Selector } from "testcafe";
+
+//prettier-ignore
+fixture `Send forgotten Password test`
+    .page`http://zero.webappsecurity.com/index.html`
+
+    .before(async t=>{
+        //Test setup goes here
+        //await runDatabaseReset()
+        //await seedTestData()
+
+    })
+
+    .beforeEach(async t=>{
+        await t.maximizeWindow();
+        await t.setTestSpeed(1);
+    })
+
+    .after(async t=>{
+        //Cleaning test Data
+        //Loggon and sending data to monitoring systems
+    })
+
+    .afterEach(async t=>{
+        //Runs after each test
+    })
+
+
+test('User want to reset password',async t=>{
+    //stared code
+    const signin_btn=Selector('#signin_button');
+    const user_form = Selector('#login_form');
+    const forgot_paswd_link=Selector('a').withText('Forgot your password ?');
+    const forgot_paswd_form =Selector('#send_password_form');
+    const user_email = Selector('#user_email');
+    const sent_pswd_btn =Selector('.btn-primary');
+    const message=Selector('div').innerText
+
+    //Action
+    await t.expect(signin_btn.exists).ok();
+    await t.click(signin_btn);
+    await t.expect(user_form.exists).ok();
+    await t.click(forgot_paswd_link);
+    await t.expect(forgot_paswd_form.exists).ok();
+    await t.typeText(user_email,'netcse02@gmail.com',{paste:true});
+    await t.click(sent_pswd_btn);
+    
+    //Assertion
+    await t.expect(message).contains('email: netcse02@gmail.com');
+
+})
--- tests/login.test.js
+++ tests/login.test.js
@@ -1,4 +1,66 @@
 import { Selector } from "testcafe";
 
 fixture `Login Test`
-    .page `https://demoqa.com/login`
+    .page `http://zero.webappsecurity.com/`
+
+    .before(async t=>{
+        //Test setup goes here
+        //await runDatabaseReset()
+        //await seedTestData()
+
+    })
+
+    .beforeEach(async t=>{
+        await t.maximizeWindow();
+        await t.setTestSpeed(1);
+    })
+
+    .after(async t=>{
+        //Cleaning test Data
+        //Loggon and sending data to monitoring systems
+    })
+
+    .afterEach(async t=>{
+        //Runs after each test
+    })
+
+    test('Login with Invalid credential',async t=>{
+        const signin_btn=Selector('#signin_button');
+        const user_form = Selector('#login_form');
+        const user_email=Selector('#user_login');
+        const use_pswd=Selector('#user_password');
+        const login=Selector('.btn-primary');
+        const err_msg=Selector('.alert-error').innerText
+
+
+        //Action
+        await t.click(signin_btn);
+        await t.expect(user_form.exists).ok();
+        await t.typeText(user_email,'netces02',{paste:true});
+        await t.typeText(use_pswd,'MAsumit1526@',{paste:true});
+        await t.expect(login.exists).ok();
+        await t.click(login)
+        await t.expect(err_msg).contains('Login and/or password are wrong.')
+
+    })
+
+
+    test('Login with Valid credential',async t=>{
+        const signin_btn=Selector('#signin_button');
+        const user_form = Selector('#login_form');
+        const user_email=Selector('#user_login');
+        const use_pswd=Selector('#user_password');
+        const login=Selector('.btn-primary');
+        const err_msg=Selector('.alert-error').innerText
+
+
+        //Action
+        await t.click(signin_btn);
+        await t.expect(user_form.exists).ok();
+        await t.typeText(user_email,'username',{paste:true});
+        await t.typeText(use_pswd,'password',{paste:true});
+        await t.expect(login.exists).ok();
+        //await t.click(login)
+        //await t.expect(err_msg).contains('Login and/or password are wrong.')
+
+    })
