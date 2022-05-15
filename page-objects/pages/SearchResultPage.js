f6a335d6f39fe031711061cae4c36b9a1da13c27
Masum Raza <masumrazait@gmail.com>
2022-05-15 23:36:26 +0530

    Added SearchResultPage Class

---
+++ page-objects/pages/SearchResultPage.js
@@ -0,0 +1,10 @@
+import { Selector } from "testcafe";
+
+class SearchResultPage {
+    constructor() {
+        this.result_tittle = Selector('h2')
+        this.search_result = Selector('div')
+
+    }
+}
+export default SearchResultPage
--- tests/search.text.js
+++ tests/search.text.js
@@ -1,22 +1,19 @@
 import { Selector } from "testcafe";
 import Navbar from '../page-objects/components/Navbar'
+import SearchResultPage from "../page-objects/pages/SearchResultPage"
+
 
 const navbar = new Navbar();
+const searchResultPage = new SearchResultPage();
 
 //prettier-ignore
 fixture`Search Navigation Funtionality`
     .page`http://zero.webappsecurity.com/index.html`
 
-test('User can user search navigation bar', async t => {
-    //Selector
-    const result_tittle = Selector('h2')
-    const search_result = Selector('div').innerText
-
-    //Action 
-    navbar.search('online bank');
-    
-    //Assertion
-    await t.expect(result_tittle.exists).ok()
-    await t.expect(navbar.searchBox.exists).ok()
-    await t.expect(search_result).contains('Zero - Free Access to Online Banking')
-})
+test('User can user search navigation bar',
+    async t => {
+        navbar.search('online bank')
+        await t.expect(searchResultPage.result_tittle.exists).ok()
+        await t.expect(navbar.searchBox.exists).ok()
+        await t.expect(searchResultPage.search_result.innerText).contains('Zero - Free Access to Online Banking')
+    })
