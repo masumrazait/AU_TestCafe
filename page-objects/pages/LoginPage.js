ba7b4992667ef597219a1600968d053eef26b6dc
Masum Raza <masumrazait@gmail.com>
2022-05-16 14:25:47 +0530

    added feedbackPage

--- page-objects/pages/LoginPage.js
+++ page-objects/pages/LoginPage.js
@@ -2,6 +2,7 @@ import { Selector, t } from "testcafe";
 
 class LoginPage {
     constructor() {
+        this.loginForm=Selector('#login_form')
         this.user_email = Selector('#user_login')
         this.user_pswd = Selector('#user_password')
         this.submitButton = Selector('.btn-primary')
@@ -9,6 +10,7 @@ class LoginPage {
     }
 
     async loginToApp(username, password) {
+
         await t
             .typeText(this.user_email, username, { paste: true, replace: true })
             .typeText(this.user_pswd, password, { paste: true, replace: true })
--- tests/feedback.test.js
+++ tests/feedback.test.js
@@ -1,4 +1,7 @@
 import { Selector } from "testcafe";
+import FeedbackPage from "../page-objects/pages/FeedbackPage";
+
+const feedbackPage = new FeedbackPage()
 
 //prettier-ignore
 fixture`User can shared the feedback and experience`
@@ -6,29 +9,16 @@ fixture`User can shared the feedback and experience`
 
 //first test Case User try to share feedback with invalid details 
 test('Shared feedback with invalid details', async t => {
-
-    //Selector
-    const feedback_tab = Selector('#feedback');
-    const feedback_form = Selector('.form-inputs');
-    const Name_input = Selector('#name');
-    const email_input = Selector('#email');
-    const subject_input = Selector('#subject');
-    const comment_input = Selector('#comment');
-    const send_message_btn = Selector('.btn-primary')
-    const message = Selector('.offset3.span6').innerText
-
-
-    //Action
-    await t.click(feedback_tab);
-    await t.expect(feedback_form.exists).ok();
-    await t.typeText(Name_input, 'AutomationTester', { paste: true });
-    await t.typeText(email_input, 'netcse02@gmail.com', { paste: true })
-    await t.typeText(subject_input, 'user can not login successfully', { paste: true });
-    await t.typeText(comment_input, 'Hi Team am try to loging my account with valid credential but am unable to loging and also i am getting error message', { paste: true });
-    await t.expect(send_message_btn.exists).ok();
-    await t.click(send_message_btn);
+    await t.click(feedbackPage.feedback_tab);
+    await t.expect(feedbackPage.feedback_form.exists).ok();
+    await t.typeText(feedbackPage.Name_input, 'AutomationTester', { paste: true });
+    await t.typeText(feedbackPage.email_input, 'netcse02@gmail.com', { paste: true })
+    await t.typeText(feedbackPage.subject_input, 'user can not login successfully', { paste: true });
+    await t.typeText(feedbackPage.comment_input, 'Hi Team am try to loging my account with valid credential but am unable to loging and also i am getting error message', { paste: true });
+    await t.expect(feedbackPage.send_message_btn.exists).ok();
+    await t.click(feedbackPage.send_message_btn);
 
     //Assertion
-    await t.expect(message).contains('Thank you for your comments, AutomationTester.')
+    await t.expect(feedbackPage.message.innerText).contains('Thank you for your comments, AutomationTester.')
 
 })
